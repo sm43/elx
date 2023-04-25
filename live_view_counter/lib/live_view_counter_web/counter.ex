eb0966147b357a6a432d7f1056f15c173e9a769f
Shivam Mukhade <shivam.mukhade@wooga.net>
2023-04-25 11:06:07 +0200

    live_view_counter: new browser starting at 0

--- live_view_counter/lib/live_view_counter_web/counter.ex
+++ live_view_counter/lib/live_view_counter_web/counter.ex
@@ -1,28 +1,36 @@
 defmodule LiveViewCounterWeb.Counter do
   use LiveViewCounterWeb, :live_view
 
-  def mount(_params, _session, socket) do
+  @topic "live"
+
+  def mount(_session, _params, socket) do
+    LiveViewCounterWeb.Endpoint.subscribe(@topic) # subscribe to the channel
     {:ok, assign(socket, :val, 0)}
   end
 
-  def handle_event("inc", _, socket) do
-    {:noreply, update(socket, :val, &(&1 + 1))}
+  def handle_event("inc", _value, socket) do
+    new_state = update(socket, :val, &(&1 + 1))
+    LiveViewCounterWeb.Endpoint.broadcast_from(self(), @topic, "inc", new_state.assigns)
+    {:noreply, new_state}
   end
 
   def handle_event("dec", _, socket) do
-    {:noreply, update(socket, :val, &(&1 - 1))}
+    new_state = update(socket, :val, &(&1 - 1))
+    LiveViewCounterWeb.Endpoint.broadcast_from(self(), @topic, "dec", new_state.assigns)
+    {:noreply, new_state}
+  end
+
+  def handle_info(msg, socket) do
+    {:noreply, assign(socket, val: msg.payload.val)}
   end
 
   def render(assigns) do
     ~H"""
     <div>
-    <h1 class="text-4xl font-bold text-center"> The count is: <%= @val %> </h1>
-
-    <p class="text-center">
-     <.button phx-click="dec">-</.button>
-     <.button phx-click="inc">+</.button>
-     </p>
-     </div>
+      <h1>The count is: <%= @val %></h1>
+      <.button phx-click="dec">-</.button>
+      <.button phx-click="inc">+</.button>
+    </div>
     """
   end
 end
